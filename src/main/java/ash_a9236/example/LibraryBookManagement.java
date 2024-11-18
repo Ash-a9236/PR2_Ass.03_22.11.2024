c630bf183dc4518dab613307108c65c4d56e4033
Loading... <158498496+Ash-a9236@users.noreply.github.com>
2024-11-18 16:03:30 -0500

    <LibraryBookManagement> class with fields

---
+++ src/main/java/ash_a9236/example/LibraryBookManagement.java
@@ -0,0 +1,42 @@
+package ash_a9236.example;
+
+import java.util.ArrayList;
+
+public class LibraryBookManagement {
+    private ArrayList<Book> books;
+
+    public LibraryBookManagement() {
+        books = new ArrayList<>();
+    }
+
+
+
+    /*
+    Design a menu-driven program that allows users to:
+
+        o Display All Books Sorted by Title:
+                Sorts and displays the books alphabetically by title.
+
+        o Display All Books Sorted by Year:
+                Sorts and displays the books in ascending order by publication year.
+
+        o Filter Books by Author:
+                Displays all books written by a specified author.
+
+        o Filter Books by Year Range:
+                Displays all books published within a specified range of years.
+
+        o Add a New Book:
+                Allows the user to add a new book to the collection.
+
+        o Remove a Book:
+                Allows the user to remove a book from the collection by title.
+
+        o Save and Exit:
+                Saves all changes back to the file and exits the program.
+     */
+
+
+
+
+}
